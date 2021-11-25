06ed274cd00272e21cc85fe14e9fd005899fdbc5
komal-lt <93261238+komal-lt@users.noreply.github.com>
2021-11-25 11:42:24 +0530

    Update registerCSSInterfaceWithDefaultTheme.js

--- src/utils/registerCSSInterfaceWithDefaultTheme.js
+++ src/utils/registerCSSInterfaceWithDefaultTheme.js
@@ -3,5 +3,6 @@ import CSSInterface from 'react-with-styles-interface-css';
 import registerInterfaceWithDefaultTheme from './registerInterfaceWithDefaultTheme';
 
 export default function registerCSSInterfaceWithDefaultTheme() {
+  console.log('inside');
   registerInterfaceWithDefaultTheme(CSSInterface);
 }
